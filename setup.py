759c95961b793d37271645e8d485a40a416171da
Tanzeem Agra <xerxez.in@gmail.com>
2022-10-12 21:01:57 +0530

    Youtube - dl - First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is demostrate deep learning using mlops and devops',
+    author='Xerxez Solutions',
+    license='MIT',
+)
